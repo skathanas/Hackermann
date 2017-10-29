25b42212a9209620225c918ab04b63fb0c2570d0
skathanas <32923297+skathanas@users.noreply.github.com>
2017-10-29 18:59:23 +0200

    Add files via upload

---
+++ _001_Ylesanne_1-100.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Numbrikeiss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+
+            string input = Console.ReadLine();
+            int Number1 = int.Parse(input);
+
+
+            int Number2 = 50;
+
+            bool hasNotGuessed = true;
+            while (hasNotGuessed)
+            {
+                if (Number2 < Number1)
+                {
+                    Console.WriteLine("Number on suurem");
+                    hasNotGuessed = false;
+                }
+
+                if (Number2 > Number1)
+                {
+                    Console.WriteLine("Number on väiksem");
+                    hasNotGuessed = false;
+                }
+
+
+
+            }
+
+            Console.WriteLine("Vajuta anykey'd");
+
+            Console.ReadLine();
+        }
+    }
+}
