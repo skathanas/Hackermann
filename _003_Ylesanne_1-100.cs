a91b9fb0de417272b058c4cac0ec4f93d3d0494a
skathanas <32923297+skathanas@users.noreply.github.com>
2017-10-29 20:03:40 +0200

    Add files via upload

---
+++ _003_Ylesanne_1-100.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Numbrikeiss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+
+            string input = Console.ReadLine();
+            int Number1 = int.Parse(input);
+
+
+            Random rnd = new Random();
+            int Number2 = rnd.Next(1, 100);
+
+
+            bool hasNotGuessed = true;
+            while (hasNotGuessed)
+            {
+                if (Number2 < Number1)
+                {
+                    Console.WriteLine("Number on suurem");
+                    hasNotGuessed = false;
+                }
+
+                if (Number2 > Number1)
+                {
+                    Console.WriteLine("Number on väiksem");
+                    hasNotGuessed = false;
+                }
+
+                if (Number2 == Number1)
+                {
+                    Console.WriteLine("Arvasid numbri ära!");
+                    hasNotGuessed = true;
+                    Console.WriteLine("Vajuta anykey'd");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Vajuta anykey'd");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _005_Ylesanne_1-100.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Numbrikeiss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int Number2 = rnd.Next(1, 100);
+
+            START:
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+
+            string input = Console.ReadLine();
+            int Number1 = int.Parse(input);
+
+
+            bool hasNotGuessed = true;
+            while (hasNotGuessed)
+            {
+                while (Number2 > Number1)
+                {
+                    Console.WriteLine("Number on suurem");
+                    hasNotGuessed = false;
+                    goto START;
+                }
+
+                while (Number2 < Number1)
+                {
+                    Console.WriteLine("Number on väiksem");
+                    hasNotGuessed = false;
+                    goto START;
+                }
+
+                while (Number2 == Number1)
+                {
+                    Console.WriteLine("Arvasid numbri ära!");
+                    hasNotGuessed = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Vajuta anykey'd");
+
+            Console.ReadLine();
+        }
+    }
+}
